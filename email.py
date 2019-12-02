0b11ccaf43c9f60a5a32a31cf4c3730daf2469f6
yang smith <892065502@qq.com>
2019-12-02 15:45:51 +0800

    updata time remind

--- email.py
+++ email.py
@@ -5,21 +5,23 @@ Created on Sun Sep 29 10:08:41 2019
 @author: DJ萧
 """
 import smtplib
+import time
 from email.mime.text import MIMEText
 from email.header import Header
 
-message = MIMEText('test')   # 邮件内容
-message['From'] = Header('小艾')   # 邮件发送者名字
-message['To'] = Header('mas')   # 邮件接收者名字
-message['Subject'] = Header('常规报告')   # 邮件主题
-
-mail = smtplib.SMTP()
-mail.connect("smtp.qq.com")   # 连接 qq 邮箱
-mail.login("892065502@qq.com", "lhvgtxxsrtnlbcjg")   # 账号和授权码
-mail.sendmail("892065502@qq.com", ["892065502@qq.com"], message.as_string())   # 发送账号、接收账号和邮件信息
-
-#from_addr ="892065502@qq.com"
-#password = "lhvgtxxsrtnlbcjg"
+if time.localtime(time.time()).tm_wday in range(0,4):
+    message = MIMEText('test')   # 邮件内容
+    message['From'] = Header('小艾')   # 邮件发送者名字
+    message['To'] = Header('mas')   # 邮件接收者名字
+    message['Subject'] = Header('基金时间提醒')   # 邮件主题
+    
+    mail = smtplib.SMTP()
+    mail.connect("smtp.qq.com")   # 连接 qq 邮箱
+    mail.login("892065502@qq.com", "lhvgtxxsrtnlbcjg")   # 账号和授权码
+    mail.sendmail("892065502@qq.com", ["892065502@qq.com"], message.as_string())   # 发送账号、接收账号和邮件信息
+    
+    #from_addr ="892065502@qq.com"
+    #password = "lhvgtxxsrtnlbcjg"
 
 
 
